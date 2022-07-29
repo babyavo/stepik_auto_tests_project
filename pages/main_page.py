8250125d78aa88ea36793dc99b1022734adba19c
babyavo <anhelinasiklichuk@gmail.com>
2022-07-29 19:02:39 +0300

    implement transitions between pages and refactor

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from pages.locators import MainPageLocators
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
