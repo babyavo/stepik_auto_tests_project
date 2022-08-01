1b9aafe3935db1cd052d210ab5eccc0b83f7ba38
babyavo <anhelinasiklichuk@gmail.com>
2022-08-01 14:12:46 +0300

    marker certain test and prepared code for review

--- pages/locators.py
+++ pages/locators.py
@@ -8,6 +8,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "[name = 'registration-email']")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "[name = 'registration-password1']")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "[name = 'registration-password2']")
+    REGISTER_SUBMIT_BUTTON = (By.CSS_SELECTOR, "[name = 'registration_submit']")
 
 
 class ProductPageLocators():
@@ -22,6 +26,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK_BUTTON = (By.CSS_SELECTOR, ".btn-group a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 
 class BasketPageLocators():
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,11 @@
 from .pages.product_page import ProductPage
 import pytest
 from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
+import time
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -23,6 +26,7 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_right_prise()
 
 
+@pytest.mark.need_review
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -57,6 +61,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -64,6 +69,8 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.should_be_login_link()
     page.go_to_login_page()
 
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = ProductPage(browser, link)
@@ -71,3 +78,32 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket_message()
+
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        self.email = str(time.time()) + "@fakemail.org"
+        self.password = str(time.time()) + "Aad"
+        login_page.register_new_user(self.email, self.password)
+        login_page.should_be_authorized_user()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_add_button()
+        page.add_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_right_book()
+        page.should_be_right_prise()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message1()
